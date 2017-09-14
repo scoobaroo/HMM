15f9b051cdb773ab42beeb5440ad9aac4cff9324
Eric Han <erichan@Erics-MacBook-Pro.local>
2017-09-14 02:11:54 -0700

    added printTranspose, new generate2 function for SeedGenerator

--- src/HMM/SeedGenerator.java
+++ src/HMM/SeedGenerator.java
@@ -20,4 +20,18 @@ public class SeedGenerator {
 		}
 		return probSeed;
 	}
+	public double[] generate2(int count) {
+		double[] seed = new double[count];
+		double fraction = (double) 1/count;
+		for(int i = 0; i< count; i++) {
+			seed[i] = fraction;
+		}
+		seed[seed.length-1] = fraction - 0.001;
+		seed[seed.length-2] = fraction - 0.002;
+		seed[seed.length-3] = fraction - 0.003;
+		seed[0] = fraction + 0.001;
+		seed[1] = fraction + 0.002;
+		seed[2] = fraction + 0.003;
+		return seed;
+	}
 }
